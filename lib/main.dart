1f388a6b6b6bee373000e88c49437c0981cb17a3
Caique Souza Dos Santos <c.souza.dos.santos@avanade.com>
2021-03-23 21:13:05 -0300

    adding the first screen thing with the text feature

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:chat_flutter/ui/chat_screen.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +18,16 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: "Chat Flutter",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue,
+        )
+      ),
+      home: ChatScreen(),
+    );
   }
 }
